08dccc86f79db5b82d6795412159efff16f9b05f
scott james <scottejames@gmail.com>
2016-05-04 12:27:10 +0100

    missed one

--- DayTwo/Files.rb
+++ DayTwo/Files.rb
@@ -1,1 +1,13 @@
 #!/usr/bin/ruby
+
+file = File.open("/tmp/tmp.txt", "w+")
+file.puts("this is my file")
+file.close()
+
+File.open("/tmp/tmp2.txt", "w+") { |file|
+	file.puts("this is my other file")
+}
+
+File.open("/tmp/tmp3.txt", "w+") do |file|
+	file.puts("this is my last file")
+end 
